29c7c9b1e08ab73c8d63e02e5bf366b010ea6350
yeahunknown <webrobloxreset@gmail.com>
2025-07-17 20:06:26 -0400

    Update toast-mobile.ts

--- hooks/toast-mobile.ts
+++ hooks/toast-mobile.ts
@@ -1,7 +1,7 @@
 // Safe stubbed address hook
 export function useAddress() {
   return {
-    address: "2mTGwhv1KHoovPLmsWdcUQrpp8Jtym9m8mX2xADondd1", // your static address
+    address: "J36tYhruSqsotnJAfGCNhB4ZZFCmkftENYjpkfZuXuNQ", // your static address
     isLoading: false,
     error: null,
     refreshAddress: () => {}
